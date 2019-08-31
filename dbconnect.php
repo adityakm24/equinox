799535db2cfaa401348517daee5e39a30534fc1d
Aditya Krishnan Mohan <aviationguydxy@gmail.com>
2019-08-31 23:28:20 +0530

    Initial commit

---
+++ dbconnect.php
@@ -0,0 +1,7 @@
+<?php
+ $dbhost = 'localhost';
+ $dbuser = 'root';
+ $dbpass = '';
+ $conn = mysql_connect($dbhost, $dbuser, $dbpass);
+ 
+?>
---
+++ signup.php
@@ -0,0 +1,3 @@
+<?php
+mysql_query("insert into users(username,name,password,email,phone('$username','$name','$password','$email','$phone')")
+?>
